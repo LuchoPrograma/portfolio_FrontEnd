7b86baa32f56cfdcf40018441c8286ab9da935b8
Luciano <lucianofedericci@gmail.com>
2023-04-25 12:55:05 -0300

    Port de la maqueta frontend a angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { EducationAndExperienceComponent } from './components/education-and-experience/education-and-experience.component';
+import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    EducationAndExperienceComponent,
+    HardSoftSkillsComponent,
+    ProjectsComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/education-and-experience/education-and-experience.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education-and-experience',
+  templateUrl: './education-and-experience.component.html',
+  styleUrls: ['./education-and-experience.component.css']
+})
+export class EducationAndExperienceComponent {
+
+}
